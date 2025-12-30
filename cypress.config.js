18467099ee70cf6c89fabbd055d99434236d930b
Eduardo Arturo de la Rosa SIlva <eduardodelarosasilva1990@outlook.com>
2025-12-30 17:49:26 -0600

    configuracion de allure report y scripts en packaje.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+
   e2e: {
+    baseUrl: 'https://www.demoblaze.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
+
+
     },
   },
 });
