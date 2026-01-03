a0a98438f84f3c2e8796e9647430d7f6fd466959
Eduardo Arturo de la Rosa SIlva <eduardodelarosasilva1990@outlook.com>
2026-01-02 21:14:21 -0600

    inhabilitar video post ejecucion

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   retries: 1,
+  video: false,
   e2e: {
     baseUrl: 'https://www.demoblaze.com',
     setupNodeEvents(on, config) {
