69351dae39bf8a8f9396ab69a6c859bdcbcc1079
Eduardo Arturo de la Rosa SIlva <eduardodelarosasilva1990@outlook.com>
2026-01-05 19:32:24 -0600

    Refactorizacion de Test case 05 y 06

--- cypress/e2e/tests/catalogo-y-compras.cy.js
+++ cypress/e2e/tests/catalogo-y-compras.cy.js
@@ -1,47 +1,43 @@
 import { commonPageData } from "../pages/commonPage/common-page.data";
-import { loginMethods } from "../pages/login/login.methods";
 import { Logger } from "../util/logger";
 import { loginData } from "../pages/login/login.data";
 import { commonPageMethods } from "../pages/commonPage/common-page.methods";
 import { homeMethods } from "../pages/Home/home.methods";
 
 describe(commonPageData.testSuites.catalogoYCompras, () => {
-    let username;
-    let password;
+    let username = loginData.validCredentials.username;
+    let password = loginData.validCredentials.password;
+
     before(() => {
     });
     beforeEach(() => {
-        Logger.reset();
+        Logger.step('PRE-CONDICIÓN: Iniciar sesión y preparar entorno');
+        commonPageMethods.LoginAndNavigateDemoblaseze(username, password);
     });
+    Logger.reset();
 
     it('TC05 Navegación por categoria Monitores', () => {
-        username = loginData.validCredentials.username;
-        password = loginData.validCredentials.password;
-
-        Logger.step('Iniciar sesión como usuario registrado')
-        Logger.subStep('Acceder a demoblaze y abrir login')
-        commonPageMethods.LoginAndNavigateDemoblaseze()
-        Logger.subStep('Insertando credenciales existentes y validas')
-        loginMethods.login(username, password);
 
         Logger.step('Navegar a la página de inicio')
         commonPageMethods.clickOnHomeOption()
 
         Logger.step('Seleccionar una categoría de productos en el menú de navegación')
         homeMethods.clickOnMonitorsOptions()
+
         Logger.verification('Verificar que se muestra la lista de productos correspondiente a la categoría seleccionada')
         homeMethods.verifyProductDisplay('Apple monitor 24')
         homeMethods.verifyProductDisplay('ASUS Full HD')
     });
 
+
     it('TC06 Navegación por categoria Phones', () => {
 
-        Logger.step('Acceder a demoblaze e ir a Home')
-        commonPageMethods.navigateToDemoBlaze()
+        Logger.step('Navegar a la página de inicio')
         commonPageMethods.clickOnHomeOption()
 
         Logger.subStep('Seleccionar una categoría de productos en el menú de navegación')
         homeMethods.clickOnPhonesOptions()
+
         Logger.verification('Verificar que se muestra la lista de productos correspondiente a la categoría seleccionada')
         homeMethods.verifyProductDisplay('Samsung galaxy s6')
         homeMethods.verifyProductDisplay('Nokia lumia 1520')
